658a801655aeac10ae41b45d702b0dad02fe90ac
Alex <flashlee2@qq.com>
2023-10-27 18:08:53 +0800

    use gruvbox colorschema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = false,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
