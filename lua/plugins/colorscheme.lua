31501d4aec8890791baee5341ce7e2fa6f65a099
Alex <flashlee2@qq.com>
2023-11-12 16:50:06 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,23 @@
 return {
   -- add gruvbox
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   opts = {
+  --     transparent_mode = false,
+  --   },
+  -- },
+
   {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      transparent_mode = false,
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
   },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
