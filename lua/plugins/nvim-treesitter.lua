7ac3578bbc3e80a43f34da80ec17b6e62fbe2662
Alex <flashlee2@qq.com>
2023-11-18 20:13:20 +0800

    add a keymap

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml", "typescript", "tsx", "json", "json5", "jsonc" })
+      vim.list_extend(
+        opts.ensure_installed,
+        { "bash", "ron", "rust", "toml", "typescript", "tsx", "json", "json5", "jsonc" }
+      )
     end
   end,
 }
