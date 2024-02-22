b2c66503bd7922718f19065fd0ceba65beb8642c
Alex <flashlee2@qq.com>
2024-02-22 19:00:00 +0800

    update tree-sitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,10 +2,24 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(
-        opts.ensure_installed,
-        { "bash", "ron", "rust", "toml", "typescript", "tsx", "json", "json5", "jsonc" }
-      )
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "java",
+        "xml",
+        "markdown",
+        "markdown_inline",
+        "ron",
+        "rust",
+        "go",
+        "toml",
+        "html",
+        "css",
+        "typescript",
+        "tsx",
+        "json",
+        "json5",
+        "jsonc",
+      })
     end
   end,
 }
